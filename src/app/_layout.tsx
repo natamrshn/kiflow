953e8d30a3519bf9fd495a5884ab4a47980c1ed3
natamrshn <natamrshn@gmail.com>
2025-09-11 19:00:07 +0100

    delete theme provider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,9 @@
 import 'react-native-reanimated'; // ⚡️ цей імпорт має бути найпершим
 
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 
-import { useColorScheme } from '../hooks/useColorScheme.web';
-
-
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -19,66 +14,64 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen 
-          name="index" 
-          options={{ 
-            headerShown: false 
-          }} 
-        />
-        <Stack.Screen 
-          name="course-code" 
-          options={{ 
-            headerShown: false 
-          }} 
-        />
-        <Stack.Screen 
-          name="home" 
-          options={{ 
-            headerShown: true
-          }} 
-        />
-        <Stack.Screen 
-          name="courses/index" 
-          options={{ 
-            headerShown: true 
-          }} 
-        />
-        <Stack.Screen 
-          name="courses/[id]" 
-          options={{ 
-            headerShown: true 
-          }} 
-        />
-        <Stack.Screen 
-          name="instractions" 
-          options={{ 
-            headerShown: false 
-          }} 
-        />
-        <Stack.Screen 
-          name="auth/login" 
-          options={{ 
-            headerShown: false 
-          }} 
-        />
-        <Stack.Screen 
-          name="auth/registration" 
-          options={{ 
-            headerShown: false 
-          }} 
-        />
-        <Stack.Screen 
-          name="design-system" 
-          options={{ 
-            headerShown: true,
-            title: 'Дизайн-система'
-          }} 
-        />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+    <Stack>
+      <Stack.Screen 
+        name="index" 
+        options={{ 
+          headerShown: false 
+        }} 
+      />
+      <Stack.Screen 
+        name="course-code" 
+        options={{ 
+          headerShown: false 
+        }} 
+      />
+      <Stack.Screen 
+        name="home" 
+        options={{ 
+          headerShown: true
+        }} 
+      />
+      <Stack.Screen 
+        name="courses/index" 
+        options={{ 
+          headerShown: true 
+        }} 
+      />
+      <Stack.Screen 
+        name="courses/[id]" 
+        options={{ 
+          headerShown: true 
+        }} 
+      />
+      <Stack.Screen 
+        name="instractions" 
+        options={{ 
+          headerShown: false 
+        }} 
+      />
+      <Stack.Screen 
+        name="auth/login" 
+        options={{ 
+          headerShown: false 
+        }} 
+      />
+      <Stack.Screen 
+        name="auth/registration" 
+        options={{ 
+          headerShown: false 
+        }} 
+      />
+      <Stack.Screen 
+        name="design-system" 
+        options={{ 
+          headerShown: true,
+          title: 'Дизайн-система'
+        }} 
+      />
+      <Stack.Screen name="+not-found" />
       {/* <StatusBar style="auto" /> */}
-    </ThemeProvider>
+    </Stack>
   );
 }
