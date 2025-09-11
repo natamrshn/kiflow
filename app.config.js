b8155b9db42c390a4b315fc92b32fa516d4957b6
natamrshn <natamrshn@gmail.com>
2025-09-11 12:23:44 +0100

    add expo dependencies

--- app.config.js
+++ app.config.js
@@ -2,12 +2,13 @@ export default {
   expo: {
     name: "kiflow",
     slug: "kiflow",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "kiflow",
     userInterfaceStyle: "automatic",
     newArchEnabled: true,
+    owner: "kiflowmobile",
     ios: {
       supportsTablet: true
     },
@@ -39,7 +40,7 @@ export default {
       typedRoutes: true
     },
     updates: {
-      url: "https://u.expo.dev/eb0e2192-b5ec-48ec-8215-47991c7a3e11"
+      url: "https://u.expo.dev/b46e19cd-4233-46c8-afb8-dd3de497c66b"
     },
     runtimeVersion: {
       policy: "appVersion"
@@ -48,7 +49,7 @@ export default {
       supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
       supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
       eas: {
-        projectId: "eb0e2192-b5ec-48ec-8215-47991c7a3e11"
+        projectId: "b46e19cd-4233-46c8-afb8-dd3de497c66b"
       }
     }
   }
